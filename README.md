ec3fa00d14727977b7287857629f3c9b4d1843a0
Kiyas <kiiyas@yandex.ru>
2021-08-28 06:00:33 +0500

    bage added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/g42vc2qyw1lpy8nc?svg=true)](https://ci.appveyor.com/project/kiiyas/004-unit-test-matchers)
+[![Build status](https://ci.appveyor.com/api/projects/status/ekkc055a5as48idp?svg=true)](https://ci.appveyor.com/project/kiiyas/005-oop-classes)
+
 
--- src/js/character.js
+++ src/js/character.js
@@ -9,15 +9,15 @@
 export default class Character {
   constructor(name, type) {
     if (typeof name !== 'string') {
-      throw new Error('The name must be a string!');
+      throw new Error('The name is not a string!');
     } else if (name.length < 2 || name.length > 10) {
-      throw new Error('The name must be between 2 and 10 characters!');
+      throw new Error('The name must be 2 to 10 characters!');
     }
 
-    const listOfTypes = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+    const listOfTypes = ['Bowerman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
 
     if (!listOfTypes.includes(type)) {
-      throw new Error('The type must be selected from the list of suggested types!');
+      throw new Error('The type of character must be selected from the list: Bowerman, Swordsman, Magician, Daemon, Undead, Zombie!');
     }
 
     this.name = name;
