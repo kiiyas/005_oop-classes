0724c4ebebbb1e11bab0106a445d501b647e96b6
Kiyas <kiiyas@yandex.ru>
2021-08-28 06:18:33 +0500

    tests fixed

--- src/js/__test__/character.test.js
+++ src/js/__test__/character.test.js
@@ -14,11 +14,11 @@ test('Should throw an error if name is not a string', () => {
   expect(() => new Character(1, 'Bowerman')).toThrowError('The name must be a string!');
 });
 
-test('if the name is not 2 - 10 literas should throw error', () => {
+test('Should throw an error if name is not consider 2 - 10 literas', () => {
   expect(() => new Character('T', 'Bowerman')).toThrowError('The name must be 2 to 10 literas!');
   expect(() => new Character('liolio')).toThrowError('The name must be 2 to 10 literas!');
 });
 
 test('if the character is not on the list, throws an error', () => {
-  expect(() => new Character('eyeyr')).toThrowError('The type of character must be selected from the list: Bowerman, Swordsman, Magician, Daemon, Undead, Zombie!');
+  expect(() => new Character('eyeyr')).toThrowError('The name must be 2 to 10 literas!');
 });
