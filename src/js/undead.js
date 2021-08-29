7097590bd3ce823a0053a6a64d35a84fb3eacc4d
Kiyas <kiiyas@yandex.ru>
2021-08-29 18:56:43 +0500

    arguments have been fixed

--- src/js/undead.js
+++ src/js/undead.js
@@ -1,8 +1,8 @@
 import Character from './character';
 
 export default class Undead extends Character {
-  constructor(name) {
-    super(name, type = 'Undead');
+  constructor(name, type = 'Undead') {
+    super(name, type);
     this.attack = 25;
     this.defens = 25;
   }
